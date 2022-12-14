0cf6a282410ee888b59aa5a5e43e7c2052405ccb
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-14 11:15:01 -0500

    Updates

---
+++ precache-manifest.9506261fe7f9277e171dc6cc90488075.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf08a7c80b855d35c785b684cf778914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f204515e7010e9e680f4",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "bd10db4edd58a9cc1df1",
+    "url": "/static/css/main.93288adf.chunk.css"
+  },
+  {
+    "revision": "f204515e7010e9e680f4",
+    "url": "/static/js/2.151b16ad.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.151b16ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd10db4edd58a9cc1df1",
+    "url": "/static/js/main.dd6419df.chunk.js"
+  },
+  {
+    "revision": "e70534e27df59ea6587d",
+    "url": "/static/js/runtime-main.72a89dc5.js"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "d04f3367c62b756d5ec29a2518fe5fc9",
+    "url": "/static/media/carousel.d04f3367.jpg"
+  },
+  {
+    "revision": "0cb86e2e5fb7075be911d0997142a854",
+    "url": "/static/media/myPic.0cb86e2e.jpg"
+  }
+]);
