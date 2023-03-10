c5ed68fd4528a1a4d8dbe07431499bf4e300f3b3
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-03-10 13:50:26 -0500

    sortBy updated

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -47,7 +47,7 @@ const config = {
   ],
   github: {
     username: 'kevindarbydev', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    sortBy: 'updated', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
