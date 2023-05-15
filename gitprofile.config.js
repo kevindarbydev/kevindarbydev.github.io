0c4782a6c60fcf83d323dd8c149942d1bd99255b
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-05-15 17:35:51 -0400

    update x list

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -63,6 +63,8 @@ const config = {
         'Custom-Laravel-Friends',
         'ReactExpenseTracker',
         'HelloWorldReactNative',
+        'Clone-Test',
+        'Gtr-Nextjs',
         'SpringBootMarketplace',
         'WebDev2_ClassExerciseOne',
         'runescapeChickenKillerScript',
