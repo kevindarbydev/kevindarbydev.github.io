e4d095ef3e70f06a4673679d5190aae6c17b7c58
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-05-17 16:31:09 -0400

    quick fix

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -33,7 +33,7 @@ const config = {
     {
       title: 'Regex Records',
       description:
-        'Discogs clone made with Laravel and React, users can add albums to their collections with real data fetched from Discogs API. Users can also add each other as friends and send messages, as well as sell and purchase albums, with Stripe and PayPal checkout. CURRENTLY MIGRATING',
+        'Discogs clone made with Laravel and React, users can add albums to their collections with real data fetched from Discogs API. Users can also add each other as friends and send messages, as well as sell and purchase albums, with Stripe and PayPal checkout.',
       imageUrl: 'https://i.imgur.com/tH3qq3M.jpg',
       link: 'http://calm-springs-25332.herokuapp.com',
     },
